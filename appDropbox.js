de08e5c2d8e3f303815da6bc660a8453e24df29c
faonbr <faonbr@gmail.com>
2022-10-26 19:00:26 -0300

    sample app for Dropbox

---
+++ appDropbox.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+app.use(express.json());
+const port = 3000;
+
+app.all('/*', function (req, res) {
+   console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
+   console.log("Body:"+ JSON.stringify(req.body, null, 3));
+
+   if(req.query.challenge!=null){
+      console.log("Query challenge: "+ req.query.challenge);
+      //When you enable a webhook in Dropbox, Dropbox makes a one-time get call to the app
+      //sending a challenge query parameter value and expects the app to respond with this value.
+      res.type('txt');
+      res.send(req.query.challenge);
+   }else{
+      //For all the rest of the requests the app responds the same message.
+      res.json({ message: "Thank you for the message" });
+   }
+})
+
+app.listen(port, function () {
+   console.log(`Example Dropbox app listening at ${port}`)
+})
