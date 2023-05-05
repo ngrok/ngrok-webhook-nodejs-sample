fb5c5c5c187de9d751831c432739e1e681990c7b
Damien Tan <dtjldamien@gmail.com>
2023-05-05 20:02:17 +0800

    chore: add logging

--- appZoom.js
+++ appZoom.js
@@ -14,23 +14,26 @@ app.all('/*', function (req, res) {
    console.log("Body:" + JSON.stringify(req.body, null, 3));
 
    if (req.body.event != null) {
-      const hmac = crypto.createHmac('sha256', secretToken);
       const body = JSON.stringify(req.body);
-      console.log('Body: ' + body);
       const timestamp = req.headers['x-zm-request-timestamp'];
-      const hashedMessage = hmac.update(`v0:${timestamp}:${body}`).digest('hex');
+      const hashedMessage = crypto.createHmac('sha256', secretToken).update(`v0:${timestamp}:${body}`).digest('hex');
+
+      console.log('Timestamp: ' + timestamp);
+      console.log('Body: ' + body);
+      console.log('Expected HMAC: ' + req.headers['x-zm-signature']);
+      console.log('Output HMAC: ' + `v0=${hashedMessage}`);
+
       if (req.headers['x-zm-signature'] == `v0=${hashedMessage}`) {
          console.log("Request is verified");
          if (req.body.event == "endpoint.url_validation") {
             const plainToken = req.body.payload.plainToken;
-            const encryptedToken = hmac.update(plainToken).digest('hex');
+            const encryptedToken = crypto.createHmac('sha256', secretToken).update(plainToken).digest('hex');
             const response = { "plainToken": plainToken, "encryptedToken": encryptedToken };
             res.json(response);
+            console.log("Endpoint URL is validated with response: " + JSON.stringify(response));
          }
       } else {
          console.log("Request is not verified");
-         console.log('Expected HMAC: ' + req.headers['x-zm-signature']);
-         console.log('Output HMAC: ' + `v0=${hashedMessage}`);
       }
    }
 })
